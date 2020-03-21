9be94321df5d6c5e61289bbe04650766624234d7
Raju <vasanthraj@Manjus-MacBook-Air.local>
2020-03-21 18:24:06 +0530

    Update in Dockerfile , run_kubernetes.sh and upload_docker.sh files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -3,16 +3,15 @@
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
+# Docker ID/path
+dockerpath=9986946435/ml-api:version-1
 
 # Step 2
-# Run the Docker Hub container with kubernetes
+kubectl run ml-api --image=9986946435/ml-api:version-1 --replicas=1
 
 
 # Step 3:
-# List kubernetes pods
+kubectl get pods
 
 # Step 4:
-# Forward the container port to a host
-
+kubectl port-forward ml-api-54f688cc5c-95sbv 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,14 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-dockerpath=9986946435/ml-api:tagname
+dockerpath=9986946435/ml-api:latest
 
 # Step 2:  
 # Authenticate & tag
 docker login --username 9986946435 --password dockerhub@123
-docker tag ml-api:latest 9986946435/ml-api:version-1
+docker tag ml-api:latest 9986946435/ml-api:version-2
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
-docker push 9986946435/ml-api:version-1
+docker push 9986946435/ml-api:version-2
